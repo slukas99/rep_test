1f12b545df2544c3f9be00aef307b1c94bab52d6
lukas <lukas@10.101.1.149>
2019-01-17 09:21:37 +0100

    commit2

--- GitHubTest/src/Student.java
+++ GitHubTest/src/Student.java
@@ -2,4 +2,14 @@
 public class Student {
 String name;
 double gebJahr;
+int ects;
+public Student(String name, double gebJahr, int ects) {
+	super();
+	this.name = name;
+	this.gebJahr = gebJahr;
+	this.ects = ects;
+}
+public int getEcts() {
+	return ects;
+}
 }
